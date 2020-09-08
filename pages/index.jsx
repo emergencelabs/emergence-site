1517772f411a2df179afe41e52b461336a931646
Myles Linder <myles.linder@gmail.com>
2020-09-07 17:59:34 -0700

    add projects section to homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,6 +18,42 @@ export default function IndexPage({ allPosts }) {
             know and more is here.
           </p>
         </Preamble>
+        <PageSection title="Projects">
+          <a href="https://emergencelabs.io">
+            <div className="my-6 flex justify-around items-center border border-gray-300 bg-white rounded-lg p-4 hover:shadow-md duration-500 ease-in-out transition-shadow">
+              <img
+                src="/assets/rocket.png"
+                alt="Emergence Labs Logo"
+                className="w-16 mx-4"
+              />
+              <div className="flex-grow">
+                <h6 className="font-semibold">Emergence Labs</h6>
+                <p className="my-2 text-sm">
+                  Emergence Labs offers Salesforce Consulting and Development
+                  for emerging businesses. Our success is derived from the
+                  success of our clients.
+                </p>
+              </div>
+            </div>
+          </a>
+          <a href="https://getcustomerinterviews.com">
+            <div className="my-6 flex justify-around items-center border border-gray-300 bg-white rounded-lg p-4 hover:shadow-md duration-500 ease-in-out transition-shadow">
+              <img
+                src="/assets/customer-voice.svg"
+                alt="Customer Voice Logo"
+                className="w-16 mx-4"
+              />
+              <div className="flex-grow">
+                <h6 className="font-semibold">Customer Interviews</h6>
+                <p className="my-2 text-sm">
+                  Easily run customer interviews with a unified solution for
+                  running sessions, analyzing responses, and presenting your
+                  findings.
+                </p>
+              </div>
+            </div>
+          </a>
+        </PageSection>
         <PageSection title="All Posts">
           {allPosts.map((post) => (
             <PostPreview
